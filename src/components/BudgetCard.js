c5912b205b0c3b53a31dcc3b5864506c629d05b0
Cookie Van <cookievan03@gmail.com>
2024-06-15 14:08:23 -0500

    added conexts and started on functions for adding and deleting budgets and expenses

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -1,7 +1,14 @@
-import { Card } from "react-bootstrap";
+import { Button, Card, ProgressBar, Stack } from "react-bootstrap";
 import { currencyFormatter } from "../utils";
 
-export default function BudgetCard({ name, amount, max }) {
+export default function BudgetCard({ name, amount, max, gray }) {
+    const classNames = []
+    if (amount > max) {
+        classNames.push("bg-danger", "bg-opacity-10")
+    } else if (gray) {
+        classNames.push("bg-light")
+    }
+    
     return (
         <Card>
             <Card.Body>
@@ -14,7 +21,26 @@ export default function BudgetCard({ name, amount, max }) {
                     </span>
                     </div>
                 </Card.Title>
+                <ProgressBar 
+                    className="rounded-pill" 
+                    variant={getProgressBarVariant(amount, max)}
+                    min={0}
+                    max={max}
+                    now={amount}
+                />
+                <Stack direction = "horizontal" gap="2" className="mt-4">
+                    <Button variant="outline-primary" className="ms-auto"> Add Expense </Button>
+                    <Button variant="outline-secondary"> View Expenses</Button>
+                </Stack>
             </Card.Body>
         </Card>
     )
+}
+
+
+function getProgressBarVariant(amount, max) {
+    const ratio = amount / max
+    if (ratio < 0.5) return "primary"
+    if (ratio < 0.75) return "warning"
+    return "danger"
 }
