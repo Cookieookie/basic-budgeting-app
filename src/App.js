cb2b499f81492d708595d6f6707917cfbfb1562f
Cookie Van <cookievan03@gmail.com>
2024-06-18 12:50:09 -0500

    added/ finished Add New Budget

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import Container from 'react-bootstrap/Container'
 import { Button, Stack } from "react-bootstrap"
 import BudgetCard from './components/BudgetCard'
 import AddBudgetModal from './components/AddBudgetModal';
+import { useState } from "react"
+import { useBudgets } from './contexts/BudgetsContext';
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+  const { budgets, getBudgetExpenses } = useBudgets()
+
   return (
     <>
     <Container>
       <Stack direction = "horizontal" gap = "2" className="mb-4">
         <h1 className="me-auto">Budgets</h1>
-        <Button variant="primary">Add Budget</Button>
+        <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
+          Add Budget
+        </Button>
         <Button variant="outline-primary">Add Expense</Button>
       </Stack>
       <div 
@@ -21,15 +28,20 @@ function App() {
           alignItms: "flex-start", 
         }}
       >
-        <BudgetCard 
-          name="Entertainment" 
-          gray
-          amount={1200} 
-          max={1000}
-          ></BudgetCard>
+        {budgets.map(budget => {
+          const amount = getBudgetExpenses(budget.id).reduce((total, expense) => total + expense.amount, 0 )
+          return (
+          <BudgetCard 
+            key = {budget.id}
+            name= {budget.name}
+            amount={amount} 
+            max={budget.max}
+          />
+          )
+        })}  
       </div>
     </Container>
-    <AddBudgetModal/>
+    <AddBudgetModal show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false) } />
     </>
   );
 }
